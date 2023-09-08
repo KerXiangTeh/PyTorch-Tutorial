02e1a94de6511fb24c61cbb35f07f40e775a9b06
Ker Xiang Teh <kerxiangteh@gmail.com>
2023-09-07 20:59:12 -0500

    add GitSmart.py

---
+++ GitSmart.py
@@ -0,0 +1,9 @@
+# import numpy library
+import numpy as np
+
+# numpy array
+array = [[1,2,3],[4,5,6]]
+first_array = np.array(array) # 2x3 array
+print("Array Type: {}".format(type(first_array))) # type
+print("Array Shape: {}".format(np.shape(first_array))) # shape
+print(first_array)
